076cfd2f78ef65fe4b23fc9dca328d594ebb5017
sampeelu <modisam26@gmail.com>
2018-12-08 00:57:55 +0530

    third commit

---
+++ TestJavaConcepts/src/Sample.java
@@ -0,0 +1,18 @@
+
+public class Sample {
+			private int account_number;
+			int account_balance; 
+			    // getter method
+				public int getBalance() {					
+					System.out.println(" this is get balance ");
+			        return this.account_balance;
+			    }
+			    // setter method
+				public void setNumber(int num) {
+			        this.account_number = num;
+			    }
+			}
+
+
+
+
